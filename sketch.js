10f04e8a9b83a8a96e8db30734e8ff11207978d0
Erin McCarty <58147518+erinmccarty@users.noreply.github.com>
2022-03-24 11:49:33 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -14,10 +14,10 @@ function preload(){
 }
 
 function setup(){
-  createCanvas(1000, 800, WEBGL);
+  createCanvas(2000, 1800, WEBGL);
   
   cam=createCapture(VIDEO);
-  cam.size(1000, 800);
+  cam.size(2000, 1800);
   cam.hide();
  
 }
